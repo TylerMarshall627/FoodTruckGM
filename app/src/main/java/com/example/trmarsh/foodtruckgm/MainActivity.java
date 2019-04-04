798ac6701b9d2ca1343a40e7d6ea2576d8200107
zagibso <zgibson@live.com>
2019-04-04 15:52:36 -0500

    Fixes all the errors

--- app/src/main/java/com/example/trmarsh/foodtruckgm/MainActivity.java
+++ app/src/main/java/com/example/trmarsh/foodtruckgm/MainActivity.java
@@ -1,14 +1,116 @@
 package com.example.trmarsh.foodtruckgm;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public abstract class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private GoogleMap mMap;
+    private Marker myMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //HI TYLER
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMarkerClickListener(this);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        final Location myLoc = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+
+        double latitude = myLoc.getLatitude();
+        double longitude = myLoc.getLongitude();
+
+        // turn latlong into street address
+        Geocoder geocoder = new Geocoder(this);
+
+        List<Address> addressList = null;
+        try {
+            addressList = geocoder.getFromLocation(latitude, longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //get address and
+        Address address = addressList.get(0);
+        String ad = address.getAddressLine(0);
+        LatLng position = new LatLng(latitude, longitude);
+        myMarker = mMap.addMarker(new MarkerOptions().position(position).title(ad));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 12));
+
+        LocationListener locationListener = new LocationListener() {
+            // callback for when GPS detects movement
+            @Override
+            public void onLocationChanged(Location location) {
+                double latitude = myLoc.getLatitude();
+                double longitude = myLoc.getLongitude();
+
+                LatLng position1 = new LatLng(latitude, longitude);
+                myMarker = mMap.addMarker(new MarkerOptions().position(position1).title(""));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position1, 12));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 }
