71aa325d429b392050d9a7963839828fd96cbb84
trmarsh <tylermarshall627@gmail.com>
2019-04-09 16:56:46 -0500

    signup push needs intent

---
+++ app/src/main/java/com/example/trmarsh/foodtruckgm/SignupActivity.java
@@ -0,0 +1,63 @@
+package com.example.trmarsh.foodtruckgm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private EditText mEmail;
+    private EditText mFirstN;
+    private EditText mLastN;
+    private EditText mPassWord;
+
+    public Firebase mRef;
+    public Firebase mRefInstance;
+    public Firebase mRefInstanceEmail;
+    public Firebase mRefInstanceFirstN;
+    public Firebase mRefInstanceLastN;
+    public Firebase mRefInstancePassWord;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRef = new Firebase("https://foodtruck-38f8f.firebaseio.com/User");
+        setContentView(R.layout.activity_signup);
+
+        mEmail = (EditText) findViewById(R.id.editText);
+        mFirstN = (EditText) findViewById(R.id.editText2);
+        mLastN = (EditText) findViewById(R.id.editText3);
+        mPassWord = (EditText) findViewById(R.id.editText4);
+
+    }
+    public void onAdd (View v){
+        String email = mEmail.getText().toString();
+        String firstN = mFirstN.getText().toString();
+        String lastN = mLastN.getText().toString();
+        String passW = mPassWord.getText().toString();
+
+        mRefInstance = mRef.child(email);
+
+        mRefInstanceEmail = mRefInstance.child("Email");
+        mRefInstanceEmail.setValue(mEmail);
+        mRefInstanceFirstN = mRefInstance.child("FirstName");
+        mRefInstanceFirstN.setValue(firstN);
+        mRefInstanceLastN = mRefInstance.child("LastName");
+        mRefInstanceLastN.setValue(lastN);
+        mRefInstancePassWord = mRefInstance.child("Password");
+        mRefInstancePassWord.setValue(passW);
+
+        mEmail.setText("");
+        mFirstN.setText("");
+        mLastN.setText("");
+        mPassWord.setText("");
+
+
+    }
+
+}
