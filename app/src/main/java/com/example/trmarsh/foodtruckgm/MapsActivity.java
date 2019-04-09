1cbd99f19962f2ae2caee0a3bb0a92ffe7066678
zagibso <zgibson@live.com>
2019-04-09 16:58:58 -0500

    THIS IS BROKEN IM GOING TO WORK ON IT AND FIX IT

--- app/src/main/java/com/example/trmarsh/foodtruckgm/MapsActivity.java
+++ app/src/main/java/com/example/trmarsh/foodtruckgm/MapsActivity.java
@@ -2,7 +2,12 @@ package com.example.trmarsh.foodtruckgm;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,15 +15,26 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Firebase mRef;
+
+    private HashMap<String, ArrayList<String>> truckLocations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        Firebase.setAndroidContext(this);
+        mRef = new Firebase("https://foodtruck-38f8f.firebaseio.com/Truck");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -36,12 +52,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        System.out.println("onMapReady");
+        Log.i("DEMO", "Start of On Map Ready");
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng startLoc = new LatLng(-34, 151);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(startLoc));
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.i("DEMO", "Start of Data Change");
+                truckLocations = new HashMap<>();
+                for (DataSnapshot snap : dataSnapshot.getChildren()) {
+                    Log.i("DEMO", "Start of For Loop");
+                    ArrayList<String> item = new ArrayList<>();
+
+                    String truckName = snap.child("Name").getValue().toString();
+                    String lat = snap.child("Lat").getValue().toString();
+                    String lng = snap.child("Lng").getValue().toString();
+
+                    item.add(truckName);
+                    item.add(lat);
+                    item.add(lng);
+
+                    truckLocations.put(truckName, item);
+                }
+                Log.i("DEMO", "End of Data Change");
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        Log.i("DEMO", "Just before EntrySet For Loop");
+        for (Map.Entry<String, ArrayList<String>> entry : truckLocations.entrySet()) {
+            String truckName = entry.getKey();
+            ArrayList<String> item = entry.getValue();
+            Double lat = Double.valueOf(item.get(1));
+            Double lng = Double.valueOf(item.get(2));
+            LatLng newPin = new LatLng(lat, lng);
+            mMap.addMarker(new MarkerOptions().position(newPin).title(truckName));
+        }
     }
 }
