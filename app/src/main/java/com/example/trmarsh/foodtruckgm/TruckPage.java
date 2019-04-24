871b2afca832c67cf0a38dfcb9704767b96b2f6f
zagibso <zgibson@live.com>
2019-04-23 21:42:03 -0500

    The truck page shows real truck info based on the passed in truck

--- app/src/main/java/com/example/trmarsh/foodtruckgm/TruckPage.java
+++ app/src/main/java/com/example/trmarsh/foodtruckgm/TruckPage.java
@@ -1,9 +1,14 @@
 package com.example.trmarsh.foodtruckgm;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -18,11 +23,14 @@ public class TruckPage extends AppCompatActivity {
 
     public final static String Extra_String_TruckName = "com.example.trmarsh.foodtruckgm.truckname";
 
-    private Firebase mRef;
-
+    private TextView bigTruckname, truckDescription, truckLocation;
+    private Button instagram, twitter, facebook;
     private ListView reviewsListView;
 
     private ArrayList<String> reviews;
+    private String instagramURL = "";
+    private String twitterURL = "";
+    private String facebookURL = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +38,84 @@ public class TruckPage extends AppCompatActivity {
         setContentView(R.layout.activity_truck_page);
         thisPage = this;
 
+        bigTruckname = findViewById(R.id.lbl_bigTruckName);
+        truckDescription = findViewById(R.id.tv_truckDescription);
+        truckLocation = findViewById(R.id.tv_truckLocation);
+        instagram = findViewById(R.id.btn_instagram);
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(instagramURL));
+                startActivity(intent);
+            }
+        });
+        twitter = findViewById(R.id.btn_twitter);
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(twitterURL));
+                startActivity(intent);
+            }
+        });
+        facebook = findViewById(R.id.btn_facebook);
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(facebookURL));
+                startActivity(intent);
+            }
+        });
         reviewsListView = findViewById(R.id.lst_reviews);
 
         String truckName = getIntent().getStringExtra(Extra_String_TruckName);
-        updateReviews(truckName);
+        Firebase.setAndroidContext(this);
+        updateTruckInfo(truckName);
+    }
+
+    private void updateTruckInfo(final String truckName) {
+        Firebase truckRef = new Firebase("https://foodtruck-38f8f.firebaseio.com/Truck");
+        truckRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                reviews = new ArrayList<>();
+                for (DataSnapshot snap : dataSnapshot.getChildren()) {
+                    String sTruckName = snap.child("Name").getValue().toString();
+                    String sBio = snap.child("Bio").getValue().toString();
+                    String sFacebook = snap.child("Facebook").getValue().toString();
+                    String sInstagram = snap.child("Instagram").getValue().toString();
+                    String sTwitter = snap.child("Twitter").getValue().toString();
+                    String sOwner = snap.child("Owner").getValue().toString();
+                    String sLat = snap.child("Lat").getValue().toString();
+                    String sLng = snap.child("Lng").getValue().toString();
+                    if (sTruckName.equalsIgnoreCase(truckName)) {
+                        bigTruckname.setText(sTruckName);
+                        truckDescription.setText(sBio);
+
+                        //TODO get address from latlng
+                        truckLocation.setText("");
+
+                        instagramURL = sInstagram;
+                        twitterURL = sTwitter;
+                        facebookURL = sFacebook;
+                        break;
+                    }
+                }
+                updateReviews(truckName);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
     }
 
     private void updateReviews(final String truckName) {
         reviews = new ArrayList<>();
-        Firebase.setAndroidContext(this);
-        mRef = new Firebase("https://foodtruck-38f8f.firebaseio.com/Review");
-        mRef.addValueEventListener(new ValueEventListener() {
+        Firebase reviewRef = new Firebase("https://foodtruck-38f8f.firebaseio.com/Review");
+        reviewRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 reviews = new ArrayList<>();
@@ -50,7 +125,6 @@ public class TruckPage extends AppCompatActivity {
                     String reviewText = snap.child("Text").getValue().toString();
                     String reviewRating = snap.child("Rating").getValue().toString();
                     if (reviewTruck.equalsIgnoreCase(truckName)) {
-                        System.out.println("INSIDE");
                         StringBuilder strBld = new StringBuilder();
                         strBld.append(String.format("[%s] %s/10", reviewTruck, reviewRating));
                         strBld.append("\n");
